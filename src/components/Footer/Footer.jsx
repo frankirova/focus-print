1f34f31b220814ea5a44be66eb21566a55d738f0
frankirova <franciscor1905@gmail.com>
2023-04-11 13:58:24 -0300

    Agregando datos del desarrollador en <Footer />

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,35 @@
 import { FooterIconsGroup } from "./FooterIconsGroup";
 
-import { Flex } from "@chakra-ui/react";
+import { Flex, FormHelperText, Image, Text } from "@chakra-ui/react";
 import "./__footer.css";
 
 export const Footer = () => {
   return (
-    <Flex
-      justify="center"
-      align="center"
-      minH="10vh"
-      as="footer"
-      className="footer"
-      bg="black"
-    >
-      <FooterIconsGroup />
-    </Flex>
+    <>
+      <Flex
+        justify="center"
+        align="center"
+        minH="10vh"
+        as="footer"
+        className="footer"
+        bg="black"
+      >
+        <FooterIconsGroup />
+        <Flex
+          pos="absolute"
+          right="30px"
+          alignItems="center"
+          justify="center"
+          gap={2}
+          as="a"
+          href="https://github.com/frankirova"
+        >
+          <Text fontSize="sm" fontFamily="sans-serif" color="white">
+            Powered by
+          </Text>
+          <Image borderRadius="xl" w="3rem" src="assets/logo fr.png" />
+        </Flex>
+      </Flex>
+    </>
   );
 };
