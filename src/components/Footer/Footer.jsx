640ab7bc5c371efe58ff2279da3fd3464e7e823f
frankirova <franciscor1905@gmail.com>
2023-04-12 16:12:39 -0300

    Acomodando my en componentes del <Footer/>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,33 +5,34 @@ import "../../styles/__footer.css";
 
 export const Footer = () => {
   return (
-    <Box bg='black'>
-    <Container maxWidth="1200px">
-      <Flex
-        justify="space-between"
-        align="center"
-        direction={["column", "column", "row", "row"]}
-        minH="10vh"
-        as="footer"
-        className="footer"
-        bg="black"
-      >
-        <Box w="288px" />
-        <FooterIconsGroup />
+    <Box bg="black">
+      <Container maxWidth="1200px">
         <Flex
-          alignItems="center"
-          justify="center"
-          gap={2}
-          as="a"
-          href="https://github.com/frankirova"
+          justify="space-between"
+          align="center"
+          direction={["column", "column", "row", "row"]}
+          minH="10vh"
+          as="footer"
+          className="footer"
+          bg="black"
         >
-          <Text fontSize="md" fontWeight="600" color="gray">
-            Desarrollado por
-          </Text>
-          <Image borderRadius="full" w="3rem" src="assets/logo fr2.png" />
+          <Box w="288px" />
+          <FooterIconsGroup />
+          <Flex
+            alignItems="center"
+            justify="center"
+            gap={2}
+            as="a"
+            my={[2, 2, 0, 0]}
+            href="https://github.com/frankirova"
+          >
+            <Text fontSize="md" fontWeight="600" color="gray">
+              Desarrollado por
+            </Text>
+            <Image borderRadius="full" w="3rem" src="assets/logo fr2.png" />
+          </Flex>
         </Flex>
-      </Flex>
-    </Container>
+      </Container>
     </Box>
   );
 };
