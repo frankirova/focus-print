a9065a4aa26f1dcef083e45c6ee8e779f155f10a
frankirova <franciscor1905@gmail.com>
2023-04-12 15:33:55 -0300

    arreglando detalles y responsive en <Footer/>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,33 @@
 import { FooterIconsGroup } from "./FooterIconsGroup";
 
-import { Flex, FormHelperText, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import "./__footer.css";
 
 export const Footer = () => {
   return (
-    <>
+    <Flex
+      justify="space-around"
+      align="center"
+      direction={["column", "column", "row", "row"]}
+      minH="10vh"
+      as="footer"
+      className="footer"
+      bg="black"
+    >
+      <Box w="288px" />
+      <FooterIconsGroup />
       <Flex
+        alignItems="center"
         justify="center"
-        align="center"
-        minH="10vh"
-        as="footer"
-        className="footer"
-        bg="black"
+        gap={2}
+        as="a"
+        href="https://github.com/frankirova"
       >
-        <FooterIconsGroup />
-        <Flex
-          pos="absolute"
-          right="30px"
-          alignItems="center"
-          justify="center"
-          gap={2}
-          as="a"
-          href="https://github.com/frankirova"
-        >
-          <Text fontSize="sm" fontFamily="sans-serif" color="white">
-            Powered by
-          </Text>
-          <Image borderRadius="xl" w="3rem" src="assets/logo fr.png" />
-        </Flex>
+        <Text fontSize="md" fontWeight="600" color="gray">
+          Desarrollado por
+        </Text>
+        <Image borderRadius="full" w="3rem" src="assets/logo fr2.png" />
       </Flex>
-    </>
+    </Flex>
   );
 };
